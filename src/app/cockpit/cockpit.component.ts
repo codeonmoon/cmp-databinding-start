608aa20d867087bc7057e51cbd8e43725ab08475
manjari09 <manjari.kumari03@gmail.com>
2021-08-22 20:49:40 +0200

    Initial commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  //serverElements = [];
+  //newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onAddServer(serverInput: HTMLInputElement) {
+    console.log(serverInput.value);
+    this.serverCreated.emit({
+      serverName: serverInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+    // this.serverElements.push({
+    //   type: 'server',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+
+  onAddBlueprint(serverInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: serverInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+    // this.serverElements.push({
+    //   type: 'blueprint',
+    //   name: this.newServerName,
+    //   content: this.newServerContent
+    // });
+  }
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,29 @@
+import { 
+  Component, 
+  Input, 
+  OnChanges, 
+  OnInit, 
+  SimpleChanges, 
+  ViewEncapsulation
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input('srvElement') element: {type: string, name:string, content:string};
+  constructor() {console.log('constructor called!');
+   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called' + changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+}
